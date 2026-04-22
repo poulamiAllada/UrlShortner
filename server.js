ab33ffceb270f70219659d39ebd2900d655a482c
poulami <poulamidevicse@gmail.com>
2026-04-22 12:59:42 +0530

    commit 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import app from './app.js';
-import './config/db.js'; 
-import './config/cache.js'; 
+import './src/config/db.js'; 
+import './src/config/cache.js'; 
 
 const port = process.env.PORT;
 app.listen(port, () => {
